9ca4d59ac07151144a52524f2ca3227cab2f908d
melinadaniilidis <melinad@seas.upenn.edu>
2024-11-07 20:11:24 -0500

    Initial Commit

---
+++ lipus_app_nav/lipus_app_navApp.swift
@@ -0,0 +1,17 @@
+//
+//  lipus_app_navApp.swift
+//  lipus_app_nav
+//
+//  Created by Melina Daniilidis on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct lipus_app_navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
